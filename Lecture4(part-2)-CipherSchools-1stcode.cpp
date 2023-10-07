cb563818698f17c952cffaa8c33e8fb657e3db36
Ayush Jaiswal <120004824+Ayush-Jaiswal-5855@users.noreply.github.com>
2023-10-07 21:25:38 +0530

    Lecture4(part-2)-CipherSchools-1stcode.cpp

---
+++ Lecture4(part-2)-CipherSchools-1stcode.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float a,b;
+    cout<<"Give 2 floating point numbers as input"<<endl;
+    cin>>a>>b;
+    if(a>0.01 && b>a){
+        cout<<"The condition in if statement is coorect";
+    }
+    else{
+        cout<<"The condition is false ";
+    }
+  return 0;
+}
