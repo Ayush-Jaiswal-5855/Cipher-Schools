a5d915c42f1c8d95c84a358ed175d52bfb20f358
Ayush Jaiswal <120004824+Ayush-Jaiswal-5855@users.noreply.github.com>
2023-10-07 21:41:01 +0530

    Lecture4(part-2)-CipherSchools-3rdcode.cpp

---
+++ Lecture4(part-2)-CipherSchools-3rdcode.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"Give three integers as input : a,b,c"<<endl;
+    cin>>a>>b>>c;
+    if(a>b && c>d){
+        cout<<"haha";
+    }
+    else if(a>b && c<d){
+        cout<<"hehe";
+    }
+    else{
+        cout<<"huhu";
+    }
+    return 0;
+}
