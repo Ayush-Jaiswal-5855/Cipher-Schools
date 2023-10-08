a754ea9ddac9e9d4a4e6ffb034761d345a944d6b
Ayush Jaiswal <120004824+Ayush-Jaiswal-5855@users.noreply.github.com>
2023-10-08 16:53:22 +0530

    Lecture4(part-2)-CipherSchools-1stcode.cpp

--- Lecture4(part-2)-CipherSchools-1stcode.cpp
+++ Lecture4(part-2)-CipherSchools-1stcode.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
+    //1st code
     float a,b;
     cout<<"Give 2 floating point numbers as input"<<endl;
     cin>>a>>b;
@@ -10,5 +11,24 @@ int main(){
     else{
         cout<<"The condition is false ";
     }
+
+    //2nd code
+    int marks;
+    cout<<"Student, please enter the marks to calculate the grade : ";
+    cin>>marks;
+    if(marks>90){
+        cout<<"A";
+    }
+    else if(marks>80){
+        cout<<"B";
+    }
+    else if(marks>70){
+        cout<<"C";
+    }
+    else{
+        cout<<"Pass";
+    }
+
+    //3rd code
   return 0;
 }
