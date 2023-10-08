a440b338e7b1d63a6c549aa70388af10d5783728
Ayush Jaiswal <120004824+Ayush-Jaiswal-5855@users.noreply.github.com>
2023-10-08 17:07:45 +0530

    Lecture7-CipherSchools.cpp

--- Lecture7-CipherSchools1stcode.cpp
+++ Lecture7-CipherSchools1stcode.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 using namespace std;
 int main(){
+    
+    //1st code
     int password;
     cout<<"Enter the password: "<<endl;
     cin>>password;
@@ -9,5 +11,31 @@ int main(){
         cin>>password;
     }
     cout<<"The user has now entered a correct password"<<endl;
+    return 0;
+
+    //2nd code
+    int password;
+    do{
+        cin>>password;
+    }
+    while(password<999999);
+    return 0;
+
+    //3rd code
+    int password;
+    int count=0;
+    while(1){
+        cout<<"chocolates "<<endl;
+        count++;
+        if(count>100) break;
+    }
+	retun 0;
+
+    //4th code
+    int i;
+    for(i=0;  ;i++){
+        cout<<i<<" ";
+        if(i>100) break;
+    }
     return 0;
 }
