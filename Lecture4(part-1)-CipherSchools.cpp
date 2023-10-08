c5e85e41f1835b2be8286430e6b4406fbd9720d3
Ayush Jaiswal <120004824+Ayush-Jaiswal-5855@users.noreply.github.com>
2023-10-08 17:11:53 +0530

    Lecture4(part-1)-CipherSchools.cpp

--- Lecture4(part-1)-CipherSchools.cpp
+++ Lecture4(part-1)-CipherSchools.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 int main(){
 	int a,b;
-    int c,d;
-    cout<<"Input 4 integers a,b,c and d"<<endl;
-    cin>>a;
-    cin>>b;
-    cin>>c;
-    cin>>d;
-    cout<<(a>b || c>d);
+	int c,d;
+	cout<<"Input 4 integers a,b,c and d"<<endl;
+	cin>>a;
+	cin>>b;
+	cin>>c;
+	cin>>d;
+	cout<<(a>b || c>d);
+	return 0;
 }
