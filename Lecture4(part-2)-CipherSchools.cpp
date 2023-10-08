c74fbc4f0b0e93c9fe2edb60ce0d0b5f6f62769e
Ayush Jaiswal <120004824+Ayush-Jaiswal-5855@users.noreply.github.com>
2023-10-08 16:56:15 +0530

    Lecture4(part-2)-CipherSchools.cpp

--- Lecture4(part-2)-CipherSchools.cpp
+++ Lecture4(part-2)-CipherSchools.cpp
@@ -44,6 +44,20 @@ int main(){
     }
 
     //4th code
-    
+    int a;
+    cout<<"enter integer a "<<endl;
+    cin>>a;
+    switch(a){
+        case 1:
+            cout<<"The value of a is 1 "<<endl;
+            break;
+        case 2:
+            cout<<"The value of a is 2 "<<endl;
+            break;
+        case 3:
+            cout<<"The value of a is 3 "<<endl;
+        default:
+            cout<<"default will be always printed";
+    }
   return 0;
 }
